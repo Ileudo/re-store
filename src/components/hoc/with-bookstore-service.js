a9673994937a41eade18909c67633a70c2b4439d
Ilya Udovichenko <rios111@yandex.ru>
2021-08-23 20:35:44 +0300

    lesson 121

---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BookstoreServiceConsumer } from '../bookstore-service-context';
+
+const withBookstoreService = () => (Wrapped) => {
+  // В компоненте высшего порядка мы создаем новый компонент.
+  // И сюда сейчас напишем код этого  компонента.
+
+  // Напомню, чтобы получить данные из Consumer нам надо передать в него render-функцию.
+  // Это функция, которая примет в качестве своего значения, тот сервис, который мы передадим
+  // через контекст.
+
+  return (props) => {
+    return (
+      <BookstoreServiceConsumer>
+        {(bookstoreService) => {
+          return <Wrapped {...props} bookstoreService={bookstoreService} />;
+        }}
+      </BookstoreServiceConsumer>
+    );
+  };
+};
+
+export default withBookstoreService;
