f9aa51cb73710fa7cfdc16f72d12732c73a1f393
Ilya Udovichenko <rios111@yandex.ru>
2021-08-25 18:33:38 +0300

    lesson 132 naming convention

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
@@ -49,13 +49,7 @@ const mapStateToProps = ({ books, loading, error }) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   const { bookstoreService } = ownProps;
   return {
-    fetchBooks: () => {
-      dispatch(booksRequested());
-      bookstoreService
-        .getBooks()
-        .then((data) => dispatch(booksLoaded(data)))
-        .catch((err) => dispatch(booksError(err)));
-    },
+    fetchBooks: fetchBooks(bookstoreService, dispatch),
   };
 };
 
